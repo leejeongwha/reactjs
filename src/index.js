102bbcc9155e4005ed9931fcb0d2f98bdaf92fa2
leejeongwha <ljk2491@naver.com>
2018-06-22 17:22:35 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+ 
+const rootElement = document.getElementById('root');
+const element = <h1>Hello, world</h1>;
+
+ReactDOM.render(<p>test</p>, rootElement);
